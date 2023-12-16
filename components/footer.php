136359ced58f7135f2a1b9da991109a9f5f69ccc
unknown <johnjacobdimaya0@gmail.com>
2023-12-16 12:37:08 +0800

    Second commit

---
+++ components/footer.php
@@ -0,0 +1,94 @@
+<?php include('../partials/__header.php'); ?>
+
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 footer-contact">
+          <h3>EduLearn</h3>
+          <p>
+            A108 Adam Street <br />
+            New York, NY 535022<br />
+            United States <br /><br />
+            <strong>Phone:</strong> +1 5589 55488 55<br />
+            <strong>Email:</strong> info@example.com<br />
+          </p>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li>
+              <i class="bx bx-chevron-right"></i> <a href="#">Home</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i> <a href="#">About us</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i> <a href="#">Services</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="#">Terms of service</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="#">Privacy policy</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Program</h4>
+            <ul>
+              <li>
+                <i class="bx bx-chevron-right"></i> <a href="#">Frontend Development</a>
+              </li>
+              <li>
+                <i class="bx bx-chevron-right"></i>
+                <a href="#">Backend Development</a>
+              </li>
+              <li>
+                <i class="bx bx-chevron-right"></i>
+                <a href="#">Mobile Development</a>
+              </li>
+              <li>
+                <i class="bx bx-chevron-right"></i> <a href="#">Java Swing</a>
+              </li>
+              <li>
+                <i class="bx bx-chevron-right"></i>
+                <a href="#">Python</a>
+              </li>
+            </ul>
+          </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Our Social Networks</h4>
+          <p>
+            Cras fermentum odio eu feugiat lide par naso tierra videa magna
+            derita valies
+          </p>
+          <div class="social-links mt-3">
+            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container py-4">
+    <div class="copyright">
+      &copy; Copyright <strong><span>EduLearn</span></strong>. All Rights Reserved
+    </div>
+  </div>
+</footer>
+<!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<?php include('../partials/__footer.php'); ?>
---
+++ partials/__footer.php
@@ -0,0 +1,12 @@
+<!-- Vendor JS Files -->
+<script src="/eduLearn/vendor/aos/aos.js"></script>
+<script src="/eduLearn/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/eduLearn/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/eduLearn/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/eduLearn/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="/eduLearn/javascript/main.js"></script>
+</body>
+
+</html>
