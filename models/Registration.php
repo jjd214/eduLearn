e3990a0a2857b97ca6399bc7d05acf17902c26b3
Leinah <gtpndn@gmail.com>
2023-12-19 23:59:12 +0900

    header to login after Email verification

--- models/Registration.php
+++ models/Registration.php
@@ -1,68 +1,67 @@
 <?php
 session_start();
-require_once $_SERVER['DOCUMENT_ROOT'].'/eduLearn/vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/eduLearn/vendor/autoload.php';
 
-class Registration extends Config {
+class Registration extends Config
+{
 
-    public function studentRegistration() {
-        
-        if(isset($_POST['submit'])) { 
+    public function studentRegistration()
+    {
+
+        if (isset($_POST['submit'])) {
             $firstname = $_POST['firstname'];
             $lastname = $_POST['lastname'];
             $gender = $_POST['gender'];
             $email = $_POST['email'];
             $password = md5($_POST['password']);
             $confirmPassword = md5($_POST['confirmPassword']);
-            
-            $verifyToken = md5(rand()); 
+
+            $verifyToken = md5(rand());
 
             $otp = $this->generateOTP();
-            
-            if($this->emailExists($email) > 0) {
+
+            if ($this->emailExists($email) > 0) {
 
                 echo '<div class="alert alert-info alert-dismissible fade show" role="alert">
                         Email already exists.
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>';
-
             } else if (!$this->validatePassword($_POST['password'])) {
 
                 echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
                         Password must be at least 8 characters long.
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                       </div>';
-
             } else if (!$this->passwordsMatch($_POST['password'], $_POST['confirmPassword'])) {
 
                 echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
                         Passwords do not match.
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                       </div>';
-
             } else {
-                
+
                 $_SESSION['signup_data'] = [
                     'firstname' => $firstname,
                     'lastname' => $lastname,
                     'gender' => $gender,
                     'email' => $email,
                     'password' => $password,
-                    'verify_token' => $verifyToken, 
+                    'verify_token' => $verifyToken,
                     'otp' => $otp
                 ];
-    
-                $this->sendOTP($_SESSION['signup_data']);
 
+                $this->sendOTP($_SESSION['signup_data']);
             }
         }
     }
 
-    public function sendOTP($userData) {
+    public function sendOTP($userData)
+    {
 
         $email = $userData['email'];
-        $otp = $userData['otp']; 
+        $otp = $userData['otp'];
 
-        $transport = new \Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'); 
+        $transport = new \Swift_SmtpTransport('smtp.gmail.com', 587, 'tls');
         $transport->setUsername('edulearn.smtp@gmail.com');
         $transport->setPassword('lyiw zlem zfdx vper');
 
@@ -87,10 +86,8 @@ class Registration extends Config {
                         OTP has been sent to your email address.
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                       </div>';
-                      
-                header("Location: otp-input.php?token=" . $userData['verify_token']);
 
-               
+                header("Location: otp-input.php?token=" . $userData['verify_token']);
             } else {
                 echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
                         Failed to send the OTP.
@@ -100,17 +97,17 @@ class Registration extends Config {
         } catch (\Swift_TransportException $e) {
             echo "Message could not be sent. Mailer Error: {$e->getMessage()}";
         }
-        
     }
 
-    public function verifyOTP($otp1,$otp2,$otp3,$otp4,$otp5,$otp6) {
+    public function verifyOTP($otp1, $otp2, $otp3, $otp4, $otp5, $otp6)
+    {
 
         $storedOTP = $_SESSION['signup_data']['otp'];
         $newOTP = isset($_SESSION['newOTP']) ? $_SESSION['newOTP'] : '';
 
         $userEnteredOTP = $otp1 . $otp2 . $otp3 . $otp4 . $otp5 . $otp6;
-        
-        if($userEnteredOTP == $storedOTP || $userEnteredOTP == $newOTP) {
+
+        if ($userEnteredOTP == $storedOTP || $userEnteredOTP == $newOTP) {
 
             $this->insertUserIntoDatabase();
 
@@ -118,10 +115,11 @@ class Registration extends Config {
                     Email verification successfull.
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>';
-        
+
+            header("Location: login.php");
+
             unset($_SESSION['newOTP']);
             unset($_SESSION['signup_data']);
-            
         } else {
             echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
                     Incorrect OTP. Please try again.
@@ -130,22 +128,23 @@ class Registration extends Config {
         }
     }
 
-    public function resendOTP() {   
+    public function resendOTP()
+    {
 
-        if(isset($_POST['resendOTP'])) {
+        if (isset($_POST['resendOTP'])) {
 
             $storedEmail = $_SESSION['signup_data']['email'];
 
             $newOTP = $this->generateOTP();
-            
-            $this->newOTPGenerator($storedEmail,$newOTP);
 
+            $this->newOTPGenerator($storedEmail, $newOTP);
         }
     }
 
-    public function newOTPGenerator($email,$newOTP) {
+    public function newOTPGenerator($email, $newOTP)
+    {
 
-        $transport = new \Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'); 
+        $transport = new \Swift_SmtpTransport('smtp.gmail.com', 587, 'tls');
         $transport->setUsername('edulearn.smtp@gmail.com');
         $transport->setPassword('lyiw zlem zfdx vper');
 
@@ -183,7 +182,8 @@ class Registration extends Config {
         $_SESSION['newOTP'] = $newOTP;
     }
 
-    public function generateOTP($length = 6) {
+    public function generateOTP($length = 6)
+    {
 
         $otp = '';
         for ($i = 0; $i < $length; $i++) {
@@ -192,7 +192,8 @@ class Registration extends Config {
         return $otp;
     }
 
-    public function validatePassword($password) {
+    public function validatePassword($password)
+    {
         $minLength = 8;
 
         if (strlen($password) < $minLength) {
@@ -202,11 +203,13 @@ class Registration extends Config {
         return true;
     }
 
-    public function passwordsMatch($password, $confirmPassword) {
+    public function passwordsMatch($password, $confirmPassword)
+    {
         return $password === $confirmPassword;
     }
 
-    public function emailExists($email) {
+    public function emailExists($email)
+    {
         $connection = $this->openConnection();
         $stmt = $connection->prepare("SELECT * FROM `user_tbl` WHERE `email` = ?");
         $stmt->execute([$email]);
@@ -215,22 +218,23 @@ class Registration extends Config {
         return $result;
     }
 
-    public function insertUserIntoDatabase() {
+    public function insertUserIntoDatabase()
+    {
 
         $userData = $_SESSION['signup_data'];
-    
+
         $firstname = $userData['firstname'];
         $lastname = $userData['lastname'];
         $gender = $userData['gender'];
         $email = $userData['email'];
         $password = $userData['password'];
         $verify_token = $userData['verify_token'];
-    
+
         $connection = $this->openConnection();
         $stmt = $connection->prepare("INSERT INTO `user_tbl` (`firstname`,`lastname`,`gender`,`email`,`password`,`verify_token`) VALUES(?,?,?,?,?,?)");
         $stmt->execute([$firstname, $lastname, $gender, $email, $password, $verify_token]);
         $result = $stmt->rowCount();
-    
+
         if ($result > 0) {
             // echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
             //         User successfully registered.
@@ -244,5 +248,3 @@ class Registration extends Config {
         }
     }
 }
-
-?>
