c3ac7e37a319ef4c73a585120c3847ec3dd5077f
jjd214 <johnjacobdimaya0@gmail.com>
2023-12-21 22:28:16 +0800

    Updated sql

--- assets/edulearn.sql
+++ assets/edulearn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2023 at 07:01 AM
+-- Generation Time: Dec 21, 2023 at 03:27 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -24,6 +24,66 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin_tbl`
+--
+
+CREATE TABLE `admin_tbl` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `access` varchar(255) DEFAULT 'admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application-form_tbl`
+--
+
+CREATE TABLE `application-form_tbl` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `age` varchar(3) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
+  `verify_token` varchar(255) DEFAULT NULL,
+  `profile` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructor_tbl`
+--
+
+CREATE TABLE `instructor_tbl` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `verify_token` varchar(255) NOT NULL,
+  `age` varchar(3) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
+  `profile` text DEFAULT NULL,
+  `access` varchar(255) DEFAULT 'instructor'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `instructor_tbl`
+--
+
+INSERT INTO `instructor_tbl` (`id`, `firstname`, `lastname`, `gender`, `email`, `password`, `verify_token`, `age`, `position`, `profile`, `access`) VALUES
+(4, 'John Jacob', 'Dimaya', 'male', 'johnjacobdimaya0@gmail.com', '817b3ae38cbe924db0ba853912232d9b', 'ce5304001e36e1ef768aa1895d2e78bb', '20', 'backend', NULL, 'instructor');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_tbl`
 --
 
@@ -35,19 +95,38 @@ CREATE TABLE `user_tbl` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `verify_token` varchar(255) NOT NULL,
-  `profile` text DEFAULT NULL
+  `profile` text DEFAULT NULL,
+  `access` varchar(255) DEFAULT 'student'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_tbl`
 --
 
-INSERT INTO `user_tbl` (`id`, `firstname`, `lastname`, `gender`, `email`, `password`, `verify_token`, `profile`) VALUES
-(1, 'John Jacob', 'Dimaya', 'male', 'johnjacobdimaya0@gmail.com', '817b3ae38cbe924db0ba853912232d9b', 'ca4281d32e68d40f1e241987f9b1e496', NULL);
+INSERT INTO `user_tbl` (`id`, `firstname`, `lastname`, `gender`, `email`, `password`, `verify_token`, `profile`, `access`) VALUES
+(5, 'John Jacob', 'Dimaya', 'male', 'johnjacobdimaya0@gmail.com', '817b3ae38cbe924db0ba853912232d9b', '520c7ebd384cc1b073859b163bb27d07', NULL, 'student');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `application-form_tbl`
+--
+ALTER TABLE `application-form_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `instructor_tbl`
+--
+ALTER TABLE `instructor_tbl`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_tbl`
@@ -60,10 +139,28 @@ ALTER TABLE `user_tbl`
 --
 
 --
+-- AUTO_INCREMENT for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `application-form_tbl`
+--
+ALTER TABLE `application-form_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `instructor_tbl`
+--
+ALTER TABLE `instructor_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `user_tbl`
 --
 ALTER TABLE `user_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
